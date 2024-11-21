87ee23eba547a73c58655cc4e5e2225718b957f9
KanzzXD <151905919+kanzzet@users.noreply.github.com>
2024-11-21 18:38:58 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <PinterestDownloader />
+  </div>
+</template>
+
+<script>
+import PinterestDownloader from './components/PinterestDownloader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PinterestDownloader
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
